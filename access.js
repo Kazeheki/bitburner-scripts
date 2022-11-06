f293195e4a593a0fc88af5b8776595b19a4ac7dd
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-11-06 12:45:34 +0100

    feat: access copies files to server

--- access.js
+++ access.js
@@ -1,16 +1,27 @@
 import { all_hackable, HACK_FILES, hack_file_to_function } from "utils";
 
+const SCRIPTS = [
+    "/find/backdoorable.js",
+    "/find/server.js",
+    "/access.js",
+    "/utils.js"
+];
+
 /**
  * Get access to all hackable servers.
  *
  * @param {import("external/NetscriptDefinitions").NS} ns
  */
 export async function main(ns) {
-    all_hackable(ns).forEach(server_name => {
-        HACK_FILES
-            .filter(file => ns.fileExists(file))
-            .forEach(file => hack_file_to_function(ns, file)(server_name));
-        ns.nuke(server_name);
-        ns.tprint(`Got access to ${server_name}`);
-    })
+    all_hackable(ns)
+        .filter(server_name => server_name != "home")
+        .forEach(server_name => {
+            HACK_FILES
+                .filter(file => ns.fileExists(file))
+                .forEach(file => hack_file_to_function(ns, file)(server_name));
+            ns.nuke(server_name);
+            ns.tprint(`Got access to ${server_name}`);
+            // arm with scripts.
+            ns.scp(SCRIPTS, server_name, "home");
+        })
 }
